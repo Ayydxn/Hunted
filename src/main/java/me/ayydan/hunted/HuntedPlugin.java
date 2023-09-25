665d98671a6be6f62957ae3080dbaa39a882d8c2
Ayydan <arcanewastakenofficial@gmail.com>
2023-09-25 12:40:06 -0400

    Added a custom logger
    - Added a custom logger class.

--- src/main/java/me/ayydan/hunted/HuntedPlugin.java
+++ src/main/java/me/ayydan/hunted/HuntedPlugin.java
@@ -1,19 +1,26 @@
 package me.ayydan.hunted;
 
-import org.bukkit.ChatColor;
+import me.ayydan.hunted.utils.HuntedLogger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HuntedPlugin extends JavaPlugin
 {
+    private static final HuntedLogger LOGGER = new HuntedLogger("Hunted Core");
+
     @Override
     public void onEnable()
     {
-        this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Welcome to Hunted!");
+        LOGGER.info("Initializing Hunted v{}...", this.getPluginMeta().getVersion());
     }
 
     @Override
     public void onDisable()
     {
-        this.getServer().getConsoleSender().sendMessage(ChatColor.RED + "Shutting down Hunted...");
+        LOGGER.info("Shutting down Hunted...");
+    }
+
+    public static HuntedLogger getHuntedLogger()
+    {
+        return LOGGER;
     }
 }
---
+++ src/main/java/me/ayydan/hunted/utils/HuntedLogger.java
@@ -0,0 +1,80 @@
+package me.ayydan.hunted.utils;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.apache.logging.log4j.message.ParameterizedMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+
+public class HuntedLogger
+{
+    private final ConsoleCommandSender consoleSender;
+    private final String loggerName;
+
+    public HuntedLogger(String name)
+    {
+        this.consoleSender = Bukkit.getServer().getConsoleSender();
+        this.loggerName = name;
+    }
+
+    public void trace(String message, Object ... arguments)
+    {
+        TextComponent traceLogMessage = Component.text("")
+                .color(NamedTextColor.GRAY)
+                .append(Component.text(String.format("[TRACE] (%s) ", this.loggerName)))
+                .append(Component.text(ParameterizedMessage.format(message, arguments)));
+
+        this.consoleSender.sendMessage(traceLogMessage);
+    }
+
+    public void info(String message, Object ... arguments)
+    {
+        TextComponent infoLogMessage = Component.text("")
+                .color(NamedTextColor.GREEN)
+                .append(Component.text(String.format("[INFO] (%s) ", this.loggerName)))
+                .append(Component.text(ParameterizedMessage.format(message, arguments)));
+
+        this.consoleSender.sendMessage(infoLogMessage);
+    }
+
+    public void debug(String message, Object ... arguments)
+    {
+        TextComponent debugLogMessage = Component.text("")
+                .color(NamedTextColor.BLUE)
+                .append(Component.text(String.format("[DEBUG] (%s) ", this.loggerName)))
+                .append(Component.text(ParameterizedMessage.format(message, arguments)));
+
+        this.consoleSender.sendMessage(debugLogMessage);
+    }
+
+    public void warn(String message, Object ... arguments)
+    {
+        TextComponent warnLogMessage = Component.text("")
+                .color(NamedTextColor.YELLOW)
+                .append(Component.text(String.format("[WARN] (%s) ", this.loggerName)))
+                .append(Component.text(ParameterizedMessage.format(message, arguments)));
+
+        this.consoleSender.sendMessage(warnLogMessage);
+    }
+
+    public void error(String message, Object ... arguments)
+    {
+        TextComponent errorLogMessage = Component.text("")
+                .color(NamedTextColor.RED)
+                .append(Component.text(String.format("[ERROR] (%s) ", this.loggerName)))
+                .append(Component.text(ParameterizedMessage.format(message, arguments)));
+
+        this.consoleSender.sendMessage(errorLogMessage);
+    }
+
+    public void fatal(String message, Object ... arguments)
+    {
+        TextComponent fatalLogMessage = Component.text("")
+                .color(NamedTextColor.DARK_RED)
+                .append(Component.text(String.format("[FATAL] (%s) ", this.loggerName)))
+                .append(Component.text(ParameterizedMessage.format(message, arguments)));
+
+        this.consoleSender.sendMessage(fatalLogMessage);
+    }
+}
